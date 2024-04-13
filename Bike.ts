d48c78555c26ec27c970b2a7f478e899491ed48c
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-13 19:09:57 -0400

    Update Bike.ts

--- Bike.ts
+++ Bike.ts
@@ -1,1 +1,59 @@
+/*
+ * Bike class.
+ *
+ * @author  Curtis Edwards
+ * @version 1.0
+ * @since   2024-04-13
+ */
+import { Vehicle } from "./Vehicle"
 
+
+export class Bike extends Vehicle {
+  // variables
+  private cadence: number
+
+  /*
+  * Constructor.
+  */
+  constructor(cadence: number) {
+    this.cadence = cadence
+  }
+
+  /*
+  * This method gets the cadence.
+  */
+  public get cadence() {
+    return this.cadence
+  }
+
+  /*
+  * This method sets the LicensePlateNumber.
+  */
+  public set cadence(cadence: number) {
+    this.cadence = value
+  }
+
+  /*
+  * This method shows the status of the truck
+  */
+  public status() {
+    console.log(" -> Speed: " + this._speed)
+    console.log(" -> Max Speed: " + this.maxSpeed)
+    console.log(" -> Color: " + this.color)
+    console.log(" -> Cadence: " + this.cadence)
+  }
+
+  /*
+  * The Acceelerate method.
+  */
+  public Acceelerate(appliedPower: number) {
+    this.cadence = this.cadence + appliedPower
+  }
+
+  /*
+  * The ringBell method.
+  */
+  public ringBell(appliedPower: number) {
+    console.log("Ding ding!\n")
+  }
+}
