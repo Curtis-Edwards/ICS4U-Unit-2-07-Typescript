68af713fd30d9fb083397bd62983590479282007
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-13 19:52:29 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ console.log('Created Bmx bike.\nStatus:\n')
 bmx.status()
 
 console.log('Set the cadense to 10\n')
-bmx.cadense = 10
+bmx.cadense(10)
 bmx.status()
 
 console.log('\nAccelerate by 15:')
