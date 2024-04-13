ec2ef116f8c09e1500f141de9d1c170bb023e264
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-13 18:50:28 -0400

    Update Truck.ts

--- Truck.ts
+++ Truck.ts
@@ -1,1 +1,48 @@
 
+/*
+ * Truck class.
+ *
+ * @author  Curtis Edwards
+ * @version 1.0
+ * @since   2024-04-13
+ */
+import { Vehicle } from "./Vehicle"
+
+
+export class Truck extends Vehicle {
+  // variables
+  private LicensePlateNumber: string
+
+  /*
+  * Constructor
+  */
+  constructor(LicensePlateNumber: String) {
+    this.LicensePlateNumber = LicensePlateNumber
+  }
+
+  /*
+  * This method gets the LicensePlateNumber
+  */
+  public get LicensePlateNumber() {
+    return this.LicensePlateNumber
+  }
+
+  /*
+  * This method sets the LicensePlateNumber
+  */
+  public set LicensePlateNumber(value: String) {
+    this.LicensePlateNumber = value
+  }
+
+  /*
+  * This method changes the speed via the accelerating formula
+  */
+  public accelerate(accelerationPower: number, accelerationTime) {
+    this._speed = (accelerationPower * accelerationTime) + this._speed
+    // check if speed exceeds max speed 
+    if (this._speed > this.maxSpeed) {
+      this._speed = this.maxSpeed
+    }
+  }
+
+}
